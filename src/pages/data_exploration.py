db284bc776496bc7682bfc54cfcade0fd42f2610
likweitan <likweitan@gmail.com>
2020-10-08 23:45:46 +0800

    Added chart

--- src/pages/data_exploration.py
+++ src/pages/data_exploration.py
@@ -1,9 +1,47 @@
+from altair.vegalite.v4.schema.core import Legend
 import streamlit as st
+import altair as alt
+
+import pandas as pd
+import numpy as np
 
 
-def load() -> None:
-    st.dataframe(info_content_df.head())
+def load(data):
+    st.header('🎲 Data Exploration')
+    with st.spinner('Loading graph...'):
+        plot_play_count_graph(data[0], data[1], data[2])
 
-    st.dataframe(info_userdata_df.head())
 
-    st.dataframe(log_problem_df.head())
+def plot_play_count_graph(info_content_df: pd.DataFrame, info_userdata_df: pd.DataFrame, log_problem_df: pd.DataFrame):
+    st.header("**♟** Difficulty Frequency **♟**")
+    st.write("This page contains basic exploratory data analyses for the purpose of getting a general feeling of what the data contains.")
+
+    chart = alt.Chart(info_content_df).mark_bar().encode(
+        x='count()',
+        y='learning_stage',
+        color='difficulty',
+        order=alt.Order(
+            # Sort the segments of the bars by this field
+            'learning_stage',
+            sort='ascending'
+        )
+
+    ).properties(
+        width=1500,
+        height=200
+    )
+
+    st.altair_chart(chart, use_container_width=True)
+
+    datecount_df = (pd.to_datetime(log_problem_df['timestamp_TW'], format='%Y-%m-%d %H:%M:%S %Z')
+                    .dt.floor('d')
+                    .value_counts()
+                    .rename_axis('date')
+                    .reset_index(name='count')
+                    .sort_values('date'))
+
+    st.header('Log Problem')
+
+    st.write('From the chart below, we could see that the number of users attempt the question is getting lower.')
+
+    st.line_chart(datecount_df['count'])
--- src/pages/home.py
+++ src/pages/home.py
@@ -9,6 +9,8 @@ def load() -> None:
     """
     # st.image("https://raw.githubusercontent.com/MaartenGr/boardgame/master/images/logo_small.jpg",
     #         use_column_width = True)
+    st.title('Assessing the Readiness of HEI in Malaysia to Accept Generation Alpha')
+
     st.markdown("> A Dashboard for the HEI in Malaysia")
     st.write("The impact of technology in education is getting more common. In the UK, most of the children start to learn coding skills from the age of 5. Generation Alpha will be very different from traditional college students. Technologies will be largely driven in education and educators need to learn how to adapt to it. The traditional method of teaching and learning might not be effective and efficient for Generation Alpha. Institutional culture needs to be changed to prepare the arrival of Generation Alpha students. In a technology-driven period, students need to learn problem-solving skills to help themselves how to think not what to think, and collaboration skills to collaborate with peers around the world. What will the Generation Alpha students behave in higher education? How to define that an institution is ready to accept Generation Alpha? These questions can be answered by finding the unique pattern of generation z using predictive analytics. The proposed system is a dashboard that allows Higher Education Institution (HEI) to capture and analyse useful insights and improve decision making from the student data.")
     st.markdown("<div align='center'><br>"
